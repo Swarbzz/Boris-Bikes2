2763eb64e8f09a78ff470a898876fdbc6f969390
George Swarbrick <glswarbrick@hotmail.co.uk>
2020-02-19 14:54:28 +0000

    step 10

--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -3,3 +3,10 @@ require 'dockingstation'
 describe "DockingStation" do
   it { expect(DockingStation.new).to respond_to(:release_bike) }
 end
+
+describe "bike" do
+  it "gets a bike" do
+    bike = Bike.new
+    expect(bike).to be_working 
+  end
+end
