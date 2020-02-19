1bf45c68117144b8785f2b35d1fdca2ddb8fae21
George Swarbrick <glswarbrick@hotmail.co.uk>
2020-02-19 15:40:20 +0000

    Step 11

--- lib/dockingstation.rb
+++ lib/dockingstation.rb
@@ -1,13 +1,24 @@
 class DockingStation
+  attr_reader :bike
 
   def release_bike
     Bike.new
   end
 
+  def dock(bike)
+    @bike = bike
+  end
+
 end
 
 class Bike
+
   def working?
     true
   end
+
+  def docked?
+    true
+  end
+
 end
--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -1,12 +1,15 @@
 require 'dockingstation'
 
-describe "DockingStation" do
+describe DockingStation do
   it { expect(DockingStation.new).to respond_to(:release_bike) }
-end
 
-describe "bike" do
   it "gets a bike" do
+    expect(Bike.new).to be_working 
+  end
+
+  it "docks bike" do
     bike = Bike.new
-    expect(bike).to be_working 
+    subject.dock(bike)
+    expect(subject.bike).to eq bike
   end
 end
